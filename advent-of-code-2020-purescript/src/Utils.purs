27e98aa95d9c4721865a2937a2aaf9bf16a38726
jem <jem@longshotsystems.co.uk>
2020-12-08 12:35:43 +0000

    clean

--- advent-of-code-2020-purescript/src/Utils.purs
+++ advent-of-code-2020-purescript/src/Utils.purs
@@ -1,10 +1,34 @@
-module Main where
+module Utils where
 
 import Prelude
+import Data.Array (uncons, cons, (:))
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..), fromJust)
+import Data.Tuple (Tuple(..))
+import Partial.Unsafe (unsafePartial)
 
-import Effect (Effect)
-import Effect.Console (log)
+tryArr :: forall a. (String -> Either String a) -> Array String -> Either String (Array a)
+tryArr p s = go 0 p s
+  where
+  go n p s = case uncons s of
+    Just { head: h, tail: t } -> case p h of
+      Right x -> map (cons x) (go (n + 1) p t)
+      Left s -> Left ("Error at #" <> (show n) <> ": " <> s)
+    Nothing -> Right []
 
-main :: Effect Unit
-main = do
-  log "🍝"
+justToEither :: forall a. String -> Maybe a -> Either String a
+justToEither s ma = case ma of
+  (Just r) -> Right r
+  Nothing -> Left ("Error " <> s)
+
+toPairs :: forall a. Array a -> Maybe (Array (Tuple a a))
+toPairs arr = case arr of
+  [] -> Just []
+  a -> do
+    { head: x, tail: xs } <- uncons a
+    { head: y, tail: ys } <- uncons xs
+    rest <- toPairs ys
+    pure ((Tuple x y) : rest)
+
+unsafeFromJust :: forall a. Maybe a -> a
+unsafeFromJust = unsafePartial fromJust
